c7d27037d4cec4e3b6e6e836bd78ce1d9f0376d3
Marco Adrián Domínguez Jiménez <99039790+VampiChoco777@users.noreply.github.com>
2024-11-26 20:36:54 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import { ButtonAppBar } from './src/components/header';
 import React from "react";
 import DashboardLayoutBasic from "./src/components/skeleton";
 
